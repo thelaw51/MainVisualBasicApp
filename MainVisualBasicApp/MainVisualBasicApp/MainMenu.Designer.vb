c9774b2431b25c7b852a2de22a09b6e3b1e17100
thelaw51 <joegillard3@gmail.com>
2022-11-06 14:55:17 +0000

    Basic main menu layout done

--- MainVisualBasicApp/MainVisualBasicApp/MainMenu.Designer.vb
+++ MainVisualBasicApp/MainVisualBasicApp/MainMenu.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class MainMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout
+        '
+        'MainMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7!, 15!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
-    End Sub
+        Me.Name = "MainMenu"
+        Me.Text = "Main Menu"
+        Me.ResumeLayout(false)
+
+End Sub
 
 End Class
